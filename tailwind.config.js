88c785207745dd6717078dd850a01459015e0828
Istiak Ahamed Ashik <istiak1439@gmail.com>
2023-10-20 16:40:27 +0600

    checked

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ const withMT = require('@material-tailwind/react/utils/withMT')
 module.exports = withMT({
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    colors: {
-      navyblue: '#000053',
-    },
     extend: {},
   },
   plugins: [require('daisyui')],
